34ac5db84848e1f980fe4f504245ceec507cea4c
pavlinpetkov420 <pavlinpetkov420@gmail.com>
2023-02-04 19:12:57 +0200

    Initial commit fastAPI Course

---
+++ README.md
@@ -0,0 +1,11 @@
+# Fast API Course
+This repo contains Python API from the freeCodeCamp Course.  
+You can check this link and watch the course -> [Python API Development - Comprehensive Course for Beginners](https://www.youtube.com/watch?v=0sOvCWFmrtA&t=3325s)  
+
+Useful commands:
+```
+uvicorn app.main:app --reload
+``` 
+`app.main` - points to the app package and `main.py` file.  
+`--reload` flag is used to reload the local server when detect saved changes and apply them immediately.
+
---
+++ app/main.py
@@ -0,0 +1,91 @@
+from typing import Optional, Union
+
+from fastapi import FastAPI, Response, status, HTTPException
+from pydantic import BaseModel
+from random import randrange
+
+app: FastAPI = FastAPI()
+
+
+class Post(BaseModel):
+    # Use pydantic.BaseModel to specify what data structure and type we expect (schema declaration)
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+my_posts: list = [{'title': 'Top beaches in Florida', 'content': 'Checkout those top beaches in Florida!', 'published': True, 'rating': 2, 'id': 314}
+                  , {'title': 'old_title', 'content': 'Content', 'published': True, 'rating': 3, 'id': 315}]
+
+
+def filter_post_by_id(post_id: int):
+    for post in my_posts:
+        if post['id'] == post_id:
+            return post
+
+
+def find_post(post_id: int):
+    for i, p in enumerate(my_posts):
+        if p['id'] == post_id:
+            return i
+
+"""
+@app decorator - tells FastAPI that the function right below is in charge of handling requests that go to: 
+    - the path /
+    - using a get operation
+
+Start the App Server: uvicorn main:app [--reload - this flag is used when you save the changes to see the immediately] 
+"""
+
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to myAPI!"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": my_posts}
+
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_post(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+
+@app.get("/posts/id={post_id}")
+def get_post(post_id: int):
+    post: Union[dict, None] = filter_post_by_id(post_id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"There is no post with id {post_id}! Please provide other id!")
+    return {"post": post}
+
+
+@app.delete("/posts/id={post_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(post_id: int):
+    index: Union[int, None] = find_post(post_id)
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"There is no post with id {post_id}! Please provide other id!")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/id={post_id}")
+def update_post(post_id: int, post: Post):
+    index: int = find_post(post_id)
+
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"There is no post with id {post_id}! Please provide other id!")
+    post_dict: dict = post.dict()
+    post_dict['id'] = post_id
+    my_posts[index] = post_dict
+    return {"data": post_dict}
+
+
