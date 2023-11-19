f1134d821efad6f7a576613f3f431756e39a5a0d
pavlinpetkov420 <pavlinpetkov420@gmail.com>
2023-11-19 21:43:31 +0200

    Add CORS to API project

--- app/main.py
+++ app/main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
-from pydantic import BaseSettings
-
-import app.models as models
-from .database import engine
 from .routers import posts, users, auth, vote
-from .config import settings
 
+from fastapi.middleware.cors import CORSMiddleware
 ########################################################################################################################
 # TODO: 1.Go over the code in the project and add documentation
 
@@ -14,6 +10,17 @@ from .config import settings
 # models.Base.metadata.create_all(bind=engine)
 
 app: FastAPI = FastAPI()
+
+origins: list[str] = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # What domains API is able to talk to? e.g. google.com
+    allow_credentials=True,
+    allow_methods=['*'],  # HTTP Methods => * = all
+    allow_headers=['*']
+
+)
 
 app.include_router(posts.router)
 app.include_router(users.router)
